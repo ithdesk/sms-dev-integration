ebd99dfaf4ec3b7a5f065b631704db12884d4596
Leonardo Zabotto <leozabotto02@gmail.com>
2022-04-10 14:55:30 -0300

    feat: SEND / LIST sms endpoints

--- src/controllers/SMSController.js
+++ src/controllers/SMSController.js
@@ -1,17 +1,52 @@
 require('dotenv').config();
 
+const sequelize = require('../database/connection');
+
+const {
+  SMSSent,
+  SMSResponse,
+} = require('../database/models');
+
+const axios = require('axios');
+
 module.exports = {
   async index(req, res) {
     try {
-      return res.status(200).send("ROTA DE LISTAGEM DE SMS")
+      const messages = await SMSSent.findAll();
+      return res.status(200).json(messages);
     } catch (err) {
       return res.status(400).json(err);
     }
   },
   async create(req, res) {
+    const transaction = await sequelize.transaction();
     try {
-      return res.status(200).send("ROTA DE ENVIO DE SMS")
+      const { receiverNumber, message } = req.body;
+
+      const smsSent = await axios.post(`${process.env.SMSDEV_API_URL}/send`, {
+        key: process.env.SMSDEV_API_KEY,
+        type: 9,
+        number: receiverNumber,
+        msg: message,
+      });
+      
+      const { data } = smsSent;
+
+      const sms = await SMSSent.create({
+        receiverNumber,
+        message,
+        smsDevId: data.id,
+        situation: data.situacao,
+        description: data.descricao,
+        errorCode: data.codigo
+      }, {
+        transaction,
+      });
+    
+      await transaction.commit();
+      return res.status(200).json(sms);
     } catch (err) {
+      await transaction.rollback();
       return res.status(400).json(err);
     }
   },
--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const routes = require('./routes');
 
 const PORT = process.env.PORT;
 
-app.use(routes);
 app.use(express.json());
+app.use(routes);
 app.use(cors());
 
 app.listen(PORT, () => {
